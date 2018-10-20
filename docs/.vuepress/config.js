3e05a5f37e0ca220aec941b73916916018835e13
ChungZH <ChungZH>
2018-10-20 12:53:19 +0800

    :clap: Add README

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,23 +6,7 @@ module.exports = {
 
     themeConfig: {
         sidebar: {
-            '/1-Preparations/': [''],
-            '/2-Toolchain/': [
-                '',
-                '2-1-TerminalEnv',
-                '2-2-DevTools'
-            ],
-            '/3-VSCode/': [
-                '',
-                '3-1-Git',
-                '3-2-Python',
-                '3-3-HelpNeeded'
-            ],
-            '/4-GUI/': [''],
-            '/5-Experience/': [''],
-            '/6-Reference/': [''],
-            '/': ['']
-        },
-        lastUpdated: 'Last Updated'
+            
+        }
     }
 }
