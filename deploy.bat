7030b9a5a134890aca5a32c3f12c824378899e74
ChungZH <ChungZH>
2018-10-20 15:17:12 +0800

    :clap: deploy

---
+++ deploy.bat
@@ -0,0 +1,5 @@
+npm run docs:build
+cd docs/.vuepress/dist
+git add -A
+git commit -m ':clap: deploy'
+git push -f git@github.com:EasyHexo/Easy-Hexo.git master:gh-pages
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ var title = 'Easy Hexo 👨‍💻';
 module.exports = {
     title: title,
     description: '轻松入门 Hexo',
-
+    base: '/Easy-Hexo',
     themeConfig: {
         sidebar: {
             
