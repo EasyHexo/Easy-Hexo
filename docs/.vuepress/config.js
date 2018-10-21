047d3c3d83a184bd7ac35121c71c44cecac44eaa
ChungZH <ChungZH>
2018-10-21 13:53:03 +0800

    :momo: Add TODO

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,43 +11,52 @@ module.exports = {
         }]
     ],
     themeConfig: {
+        serviceWorker: {
+            updatePopup: true // Boolean | Object, 默认值是 undefined.
+            // 如果设置为 true, 默认的文本配置将是: 
+            // updatePopup: { 
+            //    message: "New content is available.", 
+            //    buttonText: "Refresh" 
+            // }
+        },
         nav: [{
             text: 'Hexo 安装与配置',
             link: '/1-Hexo-install-and-config/'
-          },{
+        }, {
             text: '主题安装与配置',
             items: [{
-                text: '概要',
-                link: '/2-Theme-use-and-config/'
-              },
-              {
-                text: 'Hexo-theme-next 5.x.x',
-                link: '/2-Theme-use-and-config/2-1-hexo-theme-next-5.x.x/'
-              },
-              {
-                text: 'Hexo-theme-next 6.x.x',
-                link: '/2-Theme-use-and-config/2-2-hexo-theme-next-6.x.x/'
-              },
-              {
-                text: 'Hexo-theme-yilia',
-                link: '/2-Theme-use-and-config/2-3-hexo-theme-yilia/'
-              },
-              {
-                text: 'Hexo-theme-apollo',
-                link: '/2-Theme-use-and-config/2-4-hexo-theme-apollo/'
-              }
-                ]
-          }
-        ],
+                    text: '概要',
+                    link: '/2-Theme-use-and-config/'
+                },
+                {
+                    text: 'Hexo-theme-next 5.x.x',
+                    link: '/2-Theme-use-and-config/2-1-hexo-theme-next-5.x.x/'
+                },
+                {
+                    text: 'Hexo-theme-next 6.x.x',
+                    link: '/2-Theme-use-and-config/2-2-hexo-theme-next-6.x.x/'
+                },
+                {
+                    text: 'Hexo-theme-yilia',
+                    link: '/2-Theme-use-and-config/2-3-hexo-theme-yilia/'
+                },
+                {
+                    text: 'Hexo-theme-apollo',
+                    link: '/2-Theme-use-and-config/2-4-hexo-theme-apollo/'
+                }
+            ]
+        }],
         sidebar: {
-          '/1-Hexo-install-and-config/': [''],
-          '/2-Theme-use-and-config/': [
-              //'/',
-              '',
-              '2-1-hexo-theme-next-5.x.x/',
-              '2-2-hexo-theme-next-6.x.x/',
-              '2-3-hexo-theme-yilia/',
-              '2-4-hexo-theme-apollo/'        
+            '/1-Hexo-install-and-config/': [
+                '',
+                '1-1-meet-hexo'],
+            '/2-Theme-use-and-config/': [
+                //'/',
+                '',
+                '2-1-hexo-theme-next-5.x.x/',
+                '2-2-hexo-theme-next-6.x.x/',
+                '2-3-hexo-theme-yilia/',
+                '2-4-hexo-theme-apollo/'
             ]
         }
     }
---
+++ docs/1-Hexo-install-and-config/1-1-meet-hexo.md
@@ -0,0 +1,4 @@
+---
+title: 认识 Hexo
+---
+# 认识 Hexo
