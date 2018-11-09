ce8f37a5933f0bb58e0077f27bc9362c60ab8f2a
yi-yun <tp91225@163.com>
2018-11-09 13:05:11 +0800

    :clap: add video setting bu yi-yun

---
+++ docs/.vuepress/components/deploy.vue
@@ -0,0 +1,5 @@
+<template>
+    <video width=80% controls="controls"> 
+        <source src="https://dl.sm9.top/Video/EasyHexo/%E9%83%A8%E7%BD%B2%E5%9C%A8GitHub.mp4" type="video/mp4" type="video/mp4">
+    </video>
+</template>
---
+++ docs/.vuepress/components/write.vue
@@ -0,0 +1,6 @@
+
+<template>
+    <video width=80% controls="controls"> 
+        <source src="https://dl.sm9.top/Video/EasyHexo/%E9%85%8D%E7%BD%AE%E4%B8%8E%E5%86%99%E4%BD%9C.mp4" type="video/mp4">
+    </video>
+</template>
