823e642c6da02d7647b8de4f242cfa30019324de
Chung.ZH <124694738@qq.com>
2018-12-09 06:51:45 +0000

    :blue_book: Delete a file

---
+++ docs/.vuepress/components/valine.vue
@@ -0,0 +1,17 @@
+<template>
+    <script src="//cdn1.lncld.net/static/js/3.0.4/av-min.js"></script>
+    <script src='//unpkg.com/valine/dist/Valine.min.js'></script>
+    <div id="vcomments"></div>
+    <script>
+        new Valine({
+            el: '#vcomments' ,
+            appId: 'ESvDkjHus9gVRVp3CB6EsR1o-gzGzoHsz',
+            appKey: 'VxvYP7M4pOSCguXajHYCFWey',
+            notify:true, 
+            verify:true, 
+            avatar:'mm', 
+            placeholder: '🦜 来说话啊~' 
+        });     
+
+    </script>
+</template>
