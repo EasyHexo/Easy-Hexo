c2227ae898c4896bf68efc279e753f37d415a779
Chung.ZH <124694738@qq.com>
2018-12-09 06:54:34 +0000

    :blue_book: Add valine.vue

--- docs/.vuepress/components/valine.vue
+++ docs/.vuepress/components/valine.vue
@@ -1,6 +1,8 @@
 <template>
     <script src="//cdn1.lncld.net/static/js/3.0.4/av-min.js"></script>
     <script src='//unpkg.com/valine/dist/Valine.min.js'></script>
+
+    <!-- 添加 Valine -->
     <div id="vcomments"></div>
     <script>
         new Valine({
